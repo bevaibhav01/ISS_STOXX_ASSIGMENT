08734bf509b192f4e2f3d109b9d2547276d62fbc
bevaibhav01 <vaibhavmahale55@gmail.com>
2024-12-22 12:04:16 +0530

    multithreading implementation

--- multithreading/MultithreadingDemo.java
+++ multithreading/MultithreadingDemo.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // Inventory class with shared stock count
+//synchronize block use
 class Inventory {
     private static final Logger logger = LoggerFactory.getLogger(Inventory.class);
     private int stockCount; // Shared resource: stock count
